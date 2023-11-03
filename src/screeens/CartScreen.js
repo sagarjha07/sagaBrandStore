9cd3ef29855cc1de4f2ea80c388c2ca429d512e4
SAGAR KUMAR JHA <jha8768@gmail.com>
2023-11-04 00:09:42 +0530

    Cart screen intial changes

--- src/screeens/CartScreen.js
+++ src/screeens/CartScreen.js
@@ -1,14 +1,29 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {Colors, FontFamily, FontSize, Sizes} from '../constants';
+import HomeScreen from './HomeScreen';
+import NavigationBar from '../components/common/NavigationBar';
 
 const CartScreen = () => {
   return (
-    <View>
-      <Text>Cart Details here</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Orders</Text>
     </View>
   );
 };
 
 export default CartScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    paddingHorizontal: Sizes.x3,
+    paddingTop: Sizes.x2,
+  },
+  title: {
+    color: Colors.black,
+    fontFamily: FontFamily.regular,
+    fontSize: Sizes.x4,
+  },
+});
